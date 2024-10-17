6ad67ab0fae8a8088047ae1c07f735c28be93322
asumie-code <darkeangel.1998.ia@gmail.com>
2024-10-17 20:23:10 +0100

    sketch: snake game
     - add food interaction
     - add death interaction

--- src/app/p5_sketch/snake_game.ts
+++ src/app/p5_sketch/snake_game.ts
@@ -6,16 +6,31 @@ class Snake {
     xspeed: number 
     yspeed: number 
     scl: number
+    total: number
+    tail: p5.Vector[]
     constructor(x: number, y: number, xspeed: number, yspeed: number, scl: number) {
         this.x = x 
         this.y = y 
         this.xspeed = xspeed
         this.yspeed = yspeed
         this.scl = scl
+        this.total = 1 
+        this.tail = []
     }
 
 
     update(s: p5) {
+        if(this.total === this.tail.length) {
+            for (let i = 0; i < this.tail.length - 1; i++) {
+                this.tail[i] = this.tail[i + 1]
+            }
+        }
+        this.tail[this.total - 1] = s.createVector(this.x, this.y)
+
+        // for (let i = 0; i < this.total - 1; i++) {
+        //     this.tail[i] = this.tail[i + 1] 
+        // }
+
         this.x = this.x + this.xspeed * this.scl
         this.y = this.y + this.yspeed * this.scl
 
@@ -25,13 +40,37 @@ class Snake {
 
     show(s: p5) {
         s.fill(255)
-        s.rect(this.x , this.y, this.scl , this.scl)
+        for(let i = 0; i < this.tail.length; i++) {
+            s.rect(this.tail[i].x , this.tail[i].y, this.scl , this.scl)
+        }
+       
     }
 
     dir(x: number, y: number) {
         this.xspeed = x 
         this.yspeed = y
-}
+    }
+
+    eat(food: p5.Vector, s: p5) {
+        let d = s.dist(this.x, this.y, food.x, food.y)
+        if (d < 1) {
+            this.total++
+            return true
+        } else {
+            return  false
+        }
+    }
+
+    death(s: p5) {
+        for(let i  = 0; i < this.tail.length; i++) {
+            let pos  = this.tail[i]
+            let d = s.dist(this.x, this.y, pos.x, pos.y)
+            if (d < 1) {
+                this.total = 1 
+                this.tail = []
+            }
+        }   
+    }
 
 
 }
@@ -64,14 +103,18 @@ export const sketch = (s: p5) => {
 
     s.draw = () => {
         s.background(51)
+        snake.death(s)
         snake.update(s)
         snake.show(s)
+        if(snake.eat(food, s)) pickLocation()
         s.fill(255, 0, 100)
         s.rect(food.x, food.y, scl, scl)
     }
 
 
-
+    s.mousePressed = () => {
+        snake.total++
+    }
 
     s.keyPressed = () => {
         if (s.keyCode === s.UP_ARROW) {
