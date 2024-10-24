93c5cf5ffdcca98fd1b31d20e14914f7d58f02d1
asumie-code <darkeangel.1998.ia@gmail.com>
2024-10-24 20:02:18 +0100

    sketch: space invaders
     - add flowers

--- src/app/p5_sketch/space_invaders.ts
+++ src/app/p5_sketch/space_invaders.ts
@@ -1,6 +1,24 @@
 import p5 from 'p5'
 
 
+class Flower {
+    x: number 
+    y: number 
+
+    constructor(s: p5, x: number, y: number) {
+        this.x = x
+        this.y = y
+    }
+
+
+    show(s: p5) {
+        s.fill(255, 0, 200)
+        s.ellipse(this.x, this.y , 60, 60)
+    }
+}
+
+
+
 class Ship {
     x: number 
     constructor(s: p5) {
@@ -23,16 +41,25 @@ class Ship {
 
  export const sketch = (s: p5) => {
     let ship: Ship
+    let flowers: Flower[] =  []
     s.setup = () => {
         let canvas = s.createCanvas(500, 500)
         canvas.parent('sketch-holder')
         ship = new Ship(s)
+        for (let i = 0; i < flowers.length; i++) {
+            flowers[i] = new Flower(s, i*80+80, 60)
+            
+        }
     }
 
 
     s.draw = () => {
         s.background(51)
         ship.show(s)
+        for (let i = 0; i < 5; i++) {
+            flowers[i].show(s)
+            
+        }
 
     }
 
