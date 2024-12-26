cbbd399179dabb572b47a95721fe5e9e115ec6b6
asumie-code <darkeangel.1998.ia@gmail.com>
2024-12-27 00:04:20 +0100

    sketch: solar_system
     - create planets
     - display planets
     - reset planet transformation

--- src/app/p5_sketch/solar_system.ts
+++ src/app/p5_sketch/solar_system.ts
@@ -4,15 +4,35 @@ class Planet {
     radius: number
     angle: number
     distance: number
+    planets: Planet[]
     constructor(s: p5, r: number, d: number ) {
         this.radius = r 
         this.distance = d
         this.angle = 0
+        this.planets = []
     }
 
+    spawnMoons(s: p5, total: number) {
+        for( let i = 0; i < total; i++) { 
+            this.planets.push(new Planet(s, this.radius * 0.5, s.random(75, 300) ))
+        }
+    }
+
+
     show(s: p5) {
-        s.fill(255)
+        s.push()
+        s.translate(this.distance, 0)
+        s.rotate(this.angle)
+        s.fill(255, 100)
         s.ellipse(0, 0, this.radius * 2, this.radius * 2 )
+        if(this.planets.length !== 0) {
+            for(let planet of this.planets) {
+                planet.show(s)
+                console.log(this.planets)
+            }
+
+        }
+        s.pop()
     }
 
 }
@@ -27,7 +47,8 @@ export const sketch = (s: p5) => {
         // creating a reference to the div here positions it so you can put things above and below
         // where the sketch is displayed
         canvas2.parent('sketch-holder');
-        sun = new Planet(s, 100, 0)
+        sun = new Planet(s, 50, 0)
+        sun.spawnMoons(s, 5)
     }
 
 
