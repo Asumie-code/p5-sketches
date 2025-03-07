9fd1ecc86df311db4f31e93dfd67b7e2060c58bc
asumie-code <darkeangel.1998.ia@gmail.com>
2025-03-07 18:02:18 +0100

    sketch: lorenz_attractor
     - add an offset

--- src/app/p5_sketch/lorenz_attractor.ts
+++ src/app/p5_sketch/lorenz_attractor.ts
@@ -7,15 +7,14 @@ export const sketch = (s: p5) => {
 
     // the constants in lorenz equations 
     let a: number = 10, b: number = 28, c: number = 8.0/3.0
-  
+    let points: p5.Vector[] = []
     s.setup = () => {
  
         let canvas2 = s.createCanvas(800, 600, s.WEBGL);
-
+        s.colorMode(s.HSB)
         // creating a reference to the div here positions it so you can put things above and below
         // where the sketch is displayed
         canvas2.parent('sketch-holder');
-        s.background(0)
 
        
     }
@@ -23,6 +22,8 @@ export const sketch = (s: p5) => {
 
 
     s.draw = () => {
+        s.background(0)
+
         // change in time 
         let dt: number = 0.01 
 
@@ -35,12 +36,22 @@ export const sketch = (s: p5) => {
         y = y + dy
         z = z + dz
 
-
+        points.push(new p5.Vector(x, y, z))
         // s.translate(s.width / 2 , s.height / 2)
         s.scale(5)
-        s.stroke(255)
-        s.point(x, y, z)
-
+        s.noFill()
+        s.beginShape()
+        let hu = 0
+        for(let vector of points) {
+            s.stroke(hu, 255, 255)
+            s.vertex(vector.x, vector.y, vector.z)
+            // let offset: p5.Vector = s.createVector(s.random(-1), s.random(1), s.random(0))
+            // offset.mult(0.1)
+            // vector.add(offset)
+            // hu += 1 
+            if (hu > 255) hu = 0 
+        } 
+        s.endShape()
 
     }
 
