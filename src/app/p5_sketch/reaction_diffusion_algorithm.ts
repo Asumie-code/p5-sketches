d0ff6d0b2c4b59e2ef21b59f64e44b87478d20e4
asumie-code <darkeangel.1998.ia@gmail.com>
2025-03-22 19:20:55 +0100

    sketch: reaction_diffusion_algorithm

--- src/app/p5_sketch/reaction_diffusion_algorithm.ts
+++ src/app/p5_sketch/reaction_diffusion_algorithm.ts
@@ -5,6 +5,37 @@ import p5 from "p5"
 export const sketch = (s: p5) => {
     let grid: {a: number, b: number}[][]
     let next: {a: number, b: number}[][]
+    let dA = 1
+    let dB = .5
+    let feed = 0.055 
+    let k = 0.062
+
+    function swap() {
+        let temp = grid
+        grid = next
+        next = temp
+    }
+
+
+    function laplace(x: number, y: number, key: 'a' | 'b' ) {
+        let sum = 0
+        sum += grid[x][y][key] * -1
+        sum += grid[x - 1][y][key] * .2
+        sum += grid[x + 1][y][key] * .2
+        sum += grid[x][y + 1][key] * .2
+        sum += grid[x][y - 1][key] * .2
+        sum += grid[x - 1][y - 1][key] * 0.05
+        sum += grid[x + 1][y - 1][key] * 0.05
+        sum += grid[x + 1][y + 1][key] * 0.05
+        sum += grid[x - 1][y + 1][key] * 0.05
+ 
+        return sum
+    }
+
+
+
+
+
     s.setup = () => {
  
         let canvas2 = s.createCanvas(500, 500, s.WEBGL);
@@ -17,8 +48,14 @@ export const sketch = (s: p5) => {
             grid[x] = []
             next[x] = []
             for (let y = 0 ; y < s.height; y++) {
-                grid[x][y] =  {a: s.random(1), b: s.random(1)}
-                next[x][y] =  {a: 0, b: 0}
+                grid[x][y] =  {a: 1, b: 0}
+                next[x][y] =  {a: 1, b: 0}
+            }
+        }
+
+        for(let i = 100; i < 110; i++) {
+            for (let j = 100; j < 110; j++) { 
+                grid[i][j].b = 1
             }
         }
     }
@@ -27,10 +64,19 @@ export const sketch = (s: p5) => {
 
     s.draw = () => {
         s.background(0)
-        for(let x  = 0; x < s.width; x++) {
-            for (let y = 0 ; y < s.height; y++) {
-                next[x][y].a = grid[x][y].a * 0.2
-                next[x][y].b = grid[x][y].b * 1.2
+        for(let x  = 1; x < s.width - 1; x++) {
+            for (let y = 1 ; y < s.height - 1; y++) {
+                let a = grid[x][y].a 
+                let b = grid[x][y].b 
+                next[x][y].a = a  + 
+                               (dA * laplace(x, y, 'a')) - 
+                               (a * b * b) + 
+                               feed * (1 - a)
+                next[x][y].b = b  + 
+                               (dB * laplace(x, y, 'b')) +
+                               (a * b * b) -
+                               ((k + feed) * b)
+               
             }
         }
 
@@ -47,6 +93,9 @@ export const sketch = (s: p5) => {
             }
         }
         s.updatePixels()
+
+
+        swap()
     }
 
 }
