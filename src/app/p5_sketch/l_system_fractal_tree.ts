f7cd61fa7642dec436a659fe93455a7fda903422
asumie-code <darkeangel.1998.ia@gmail.com>
2025-04-10 23:40:27 +0100

    sketch: l_system_fractal_tree
     - write generate function
     - create button

--- src/app/p5_sketch/l_system_fractal_tree.ts
+++ src/app/p5_sketch/l_system_fractal_tree.ts
@@ -6,20 +6,45 @@ import p5 from "p5"
 export const sketch = (s: p5) => {
 
     let axiom = 'A'
+    let sentence = axiom
+    let rules: {a: string, b: string}[] = []
 
-    let rule_1 = {
+     rules[0] = {
         a: 'A', 
-        b: 'AB'
+        b: 'ABC'
     }
 
-    let rule_2 = {
+    rules[1] = {
         a: 'B', 
         b: 'A'
+    }
+
+    function generate() {
+        let nextSentence = ''
+        for(let i = 0; i < sentence.length; i++) {
+            let current = sentence.charAt(i)
+            let found = false
+            for(let j = 0; j < rules.length; j++) {
+                found = true
+                if (current == rules[j].a) {
+                    nextSentence += rules[j].b
+                    break
+                } 
+            }
+            if(!found) {
+                nextSentence += current
+            }
+
+        }
+        sentence = nextSentence
+        s.createP(sentence)
     }
  
     s.setup = () => {
         s.noCanvas() 
         s.createP(axiom)
+        let button = s.createButton('genereate')
+        button.mousePressed(generate)
     }
 
 }
