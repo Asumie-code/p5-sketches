da811ebf693b24b9e8f58a513f7b3a8ce34b615b
asumie-code <darkeangel.1998.ia@gmail.com>
2025-04-12 23:39:20 +0100

    sketch: l_system_fractal_tree
     - write turtle function

--- src/app/p5_sketch/l_system_fractal_tree.ts
+++ src/app/p5_sketch/l_system_fractal_tree.ts
@@ -5,19 +5,18 @@ import p5 from "p5"
 
 export const sketch = (s: p5) => {
 
-    let axiom = 'A'
+    let axiom = 'F'
     let sentence = axiom
+    let len  = 100
     let rules: {a: string, b: string}[] = []
 
      rules[0] = {
-        a: 'A', 
-        b: 'ABC'
+        a: 'F', 
+        b: 'FF+[+F-F-F]-[-F+F+F]'
     }
 
-    rules[1] = {
-        a: 'B', 
-        b: 'A'
-    }
+
+
 
     function generate() {
         let nextSentence = ''
@@ -38,11 +37,39 @@ export const sketch = (s: p5) => {
         }
         sentence = nextSentence
         s.createP(sentence)
+        turtle()
+    }
+
+
+    function turtle() {
+        s.background(51)
+        s.translate(s.width / 2, s.height)
+        s.stroke(255)
+        for(let i = 0; i < sentence.length; i++) {
+            let current = sentence.charAt(i)
+            if(current === 'F') {
+                s.line(0, 0, 0, -len)
+                s.translate(0 , -len)
+            } else if (current === '+') {
+                s.rotate(s.PI / 6)
+            } else if (current === '-') {
+                s.rotate(-s.PI / 6)
+            } else if (current === '[') {
+                s.push()
+            } else if (current === ']') {
+                s.pop()
+            }
+        }
     }
  
     s.setup = () => {
-        s.noCanvas() 
+        let canvas2 = s.createCanvas(400, 400);
+        // creating a reference to the div here positions it so you can put things above and below
+        // where the sketch is displayed
+        canvas2.parent('sketch-holder');
+        s.background(51)
         s.createP(axiom)
+        turtle()
         let button = s.createButton('genereate')
         button.mousePressed(generate)
     }
