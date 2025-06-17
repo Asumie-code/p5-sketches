ada7e74a77c26432363b7da4c79735aaa326fb1e
asumie-code <darkeangel.1998.ia@gmail.com>
2025-06-17 17:12:42 +0100

    sketch: superellipse final

--- src/app/p5_sketch/superellipse.ts
+++ src/app/p5_sketch/superellipse.ts
@@ -2,23 +2,36 @@ import p5 from "p5"
 
 export const sketch = (s: p5) => {
 
+    let silder: p5.Element
+    function sgn(val: number): number {
+        if (val > 0) return 1 
+        else if(val < 0 ) return -1 
+        else return 0
+    }
+
     s.setup = () => {
         let canvas2 = s.createCanvas(800, 600);
         // creating a reference to the div here positions it so you can put things above and below
         // where the sketch is displayed
         canvas2.parent('sketch-holder');
+        silder = s.createSlider(0, 10, 2, 0.01)
+
     }
 
     s.draw = () => {
         s.background(51)
         s.translate(s.width / 2, s.height / 2)
-        let r = 100
+        let a = 100, b = 100, n = silder.value() as number
+
         s.stroke(255)
         s.noFill()
         s.beginShape()
-        for(let a = 0; a < s.TWO_PI; a += 0.1) {
-            let x = r * s.cos(a)
-            let y = r * s.sin(a)
+        for(let angle = 0; angle < s.TWO_PI; angle += 0.1) {
+            // let x = r * s.cos(angle)
+            // let y = r * s.sin(angle)
+            let na = 2/n
+            let x = s.pow(s.abs(s.cos(angle)), na) * a * sgn(s.cos(angle))
+            let y = s.pow(s.abs(s.sin(angle)), na) * b * sgn(s.sin(angle))
             s.vertex(x, y)
         }
         s.endShape(s.CLOSE)
